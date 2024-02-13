ca2a8ad8dfbba1f807d83513a2c55a8819a9a5bb
Suresh Pawar <sureshkumar.pawar@gmail.com>
2024-02-12 20:49:04 -0800

    upd main

--- snowflake_assistance_streamlit.py
+++ snowflake_assistance_streamlit.py
@@ -2,9 +2,9 @@ import logging
 import os
 import sys
 from typing import Any, Dict, Generator, List, Union
-
-import openai
+import requests
 import streamlit as st
+import openai  # Ensure this is added to use OpenAI's API
 from llama_index import StorageContext, load_index_from_storage
 
 # Setup logging
@@ -13,16 +13,11 @@ logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 ResponseType = Union[Generator[Any, None, None], Any, List, Dict]
 
-# Placeholder for the OpenAI API key
-openai_api_key = ""
-
-#@st.cache_resource(show_spinner=False)
 def load_index() -> Any:
     """Load the index from the storage directory."""
     print("Loading index...")
     base_dir = os.path.dirname(os.path.abspath(__file__))
     dir_path = os.path.join(base_dir, ".kb")
-
     storage_context = StorageContext.from_defaults(persist_dir=dir_path)
     index = load_index_from_storage(storage_context)
     query_engine = index.as_query_engine()
@@ -30,83 +25,79 @@ def load_index() -> Any:
     return query_engine
 
 def validate_api_key(api_key: str) -> bool:
-    """Validate the OpenAI API key. Placeholder for actual validation logic."""
-    return bool(api_key)  # Simple check to ensure the key is not empty
+    """Validate the OpenAI API key by attempting a test API call."""
+    url = "https://api.openai.com/v1/models"
+    response = requests.get(url, headers={"Authorization": f"Bearer {api_key}"})
+    if response.status_code == 200:
+        print("Valid key")
+        return True
+    else:
+        print("Invalid key")
+        return False
 
 def main() -> None:
-    """Run the chatbot."""
-    global openai_api_key
-
     st.sidebar.title("Configuration")
     openai_api_key_input = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
 
-    if openai_api_key_input:
-        if validate_api_key(openai_api_key_input):
-            openai_api_key = openai_api_key_input
-            openai.api_key = openai_api_key
-            st.sidebar.success("API Key accepted.")
-        else:
-            st.sidebar.error("Invalid API Key. Please enter a valid OpenAI API key.")
-            return
-    else:
-        st.sidebar.warning("Please enter an OpenAI API key to enable the chat.")
-        return
-
-    if "query_engine" not in st.session_state:
-        st.session_state.query_engine = load_index()
-
     st.title("Chat with Snowflake Blog AI Assistant!")
-    # Add an image (replace 'path_to_image' with your image path or URL)
-    image_path = 'blog-ai.png'  # Example: 'images/clothing.jpg' or 'http://example.com/image.jpg'
-    st.image(image_path, caption='Fashion and Apparel')
+    image_path = 'blog-ai.png'  # Ensure this path is correct or adjust as needed
+    st.image(image_path, caption='Snowflake Blog AI Assistant')
 
     st.markdown("""
     👗 **Your Assistant**
-
     All about Snowpark for Data Engineering Quickstarts from quickstarts.snowflake.com. Ask away your questions!
     I built an LLM Chatbot that uses Retrieval Augmented Generation to answer questions about a particular blog or a list of blogs from snowflake technology.
-    
+
     **How It Works:**
     Just type your query, like below \n
     how to load table into dataframe? \n
     how to add security to dataframe?
-    
     """)
 
-    st.write("All about Snowpark for Data Engineering Quickstarts from quickstarts.snowflake.com. Ask away your questions!")
+    # Enable chat box only if API key is valid
+    if openai_api_key_input and validate_api_key(openai_api_key_input):
+        openai.api_key = openai_api_key_input
+        st.sidebar.success("API Key accepted.")
 
-    if "messages" not in st.session_state:
-        system_prompt = (
-            "Your purpose is to answer questions about specific documents only. "
-            "Please answer the user's questions based on what you know about the document. "
-            "If the question is outside the scope of the document, please politely decline. "
-            "If you don't know the answer, say `I don't know`."
-        )
-        st.session_state.messages = [{"role": "system", "content": system_prompt}]
+        if "query_engine" not in st.session_state:
+            st.session_state.query_engine = load_index()
 
-    for message in st.session_state.messages:
-        if message["role"] not in ["user", "assistant"]:
-            continue
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        if "messages" not in st.session_state:
+            system_prompt = (
+                "Your purpose is to answer questions about specific documents only. "
+                "Please answer the user's questions based on what you know about the document. "
+                "If the question is outside the scope of the document, please politely decline. "
+                "If you don't know the answer, say `I don't know`."
+            )
+            st.session_state.messages = [{"role": "system", "content": system_prompt}]
 
-    if prompt := st.chat_input("Ask your question:"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+        for message in st.session_state.messages:
+            if message["role"] not in ["user", "assistant"]:
+                continue
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            print("Querying query engine API...")
-            # Since the actual response handling is not detailed, adjust according to your API response
-            response = st.session_state.query_engine.query(prompt)
-            if "Empty Response" in response.response:
-                full_response = "I do not have an answer for your query. Please try other resources on our website."
-            else:
-                full_response = response.response
+        if prompt := st.chat_input("Ask your question:"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-            message_placeholder.markdown(full_response)
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with (st.chat_message("assistant")):
+                message_placeholder = st.empty()
+                print("Querying query engine API...")
+                response = st.session_state.query_engine.query(prompt)
+                full_response = response
+                if "Empty Response" in response.response:
+                    full_response = "I do not have an answer for your query. Please try other resources on our website."
+                else:
+                    full_response = response.response
+                message_placeholder.markdown(full_response)
+                st.session_state.messages.append({"role": "assistant", "content": full_response})
+    else:
+        if openai_api_key_input:  # This condition will be true if an API key was entered but is invalid.
+            st.sidebar.error("Invalid API Key. Please enter a valid OpenAI API key.")
+        else:
+            st.sidebar.warning("Please enter an OpenAI API key to enable the chat.")
 
 if __name__ == "__main__":
     main()
